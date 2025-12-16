d34ef579e4f2e893409540fd2f75424c39d7148b
Taposh Barai <taposhbarai03@gmail.com>
2025-12-16 12:45:47 +0600

    github-and-twiter-added

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,10 +1,10 @@
 
 import { RiFacebookFill, RiInstagramFill, RiLinkedinBoxFill, RiPhoneFill, RiMailFill, RiBookLine } from "react-icons/ri";
 import { SlSocialFacebook } from "react-icons/sl";
-import { TiSocialInstagram, TiSocialLinkedin } from "react-icons/ti";
+import {TiSocialLinkedin } from "react-icons/ti";
 import { Link, NavLink } from "react-router";
 import Logo from "../../component/Logo/Logo";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -51,25 +51,35 @@ const Footer = () => {
 
         
                 <div className="flex gap-3 mt-2">
-
+             {/*Facebook Link*/}
               <NavLink to="https://www.facebook.com/share/17KrLmmkxj/">
                   <button className="p-2 rounded-full bg-blue-600 text-white hover:bg-blue-700 transition">
                     <SlSocialFacebook size={18} />
                 </button>
               </NavLink>
 
-                <button className="p-2 rounded-full bg-sky-500 text-white hover:bg-sky-600 transition">
+
+              {/*Twitter Link*/}
+               <NavLink to="https://x.com/taposh_barai03S">
+                  <button className="p-2 rounded-full bg-sky-500 text-white hover:bg-sky-600 transition">
                 
                     <FaXTwitter size={18} />
                 </button>
+               </NavLink>
 
-                <button className="p-2 rounded-full bg-pink-600 text-white hover:bg-pink-700 transition">
-                    <TiSocialInstagram size={18} />
+               <NavLink to="https://github.com/taposh25">
+
+                 <button className="p-2 rounded-full bg-pink-600 text-white hover:bg-pink-700 transition">
+                    <FaGithub  size={18} />
                 </button>
 
-                <button className="p-2 rounded-full bg-blue-700 text-white hover:bg-blue-800 transition">
+               </NavLink>
+
+                <NavLink to="">
+                  <button className="p-2 rounded-full bg-blue-700 text-white hover:bg-blue-800 transition">
                     <TiSocialLinkedin size={18} />
                 </button>
+                </NavLink>
                 
                 </div>
 
